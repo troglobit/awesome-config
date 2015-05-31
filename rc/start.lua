cdaef4939f7eaf4bd723b9204ffaa664f7f42b2c
Joachim Nilsson <troglobit@gmail.com>
2015-05-31 02:21:40 +0200

    Always start with caps as an additional ctrl, disable pidgin+numlockx.
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- rc/start.lua
+++ rc/start.lua
@@ -9,7 +9,9 @@ local execute = {
    "udisks-glue -p /run/user/$(id -u)/udisks-glue.pid",
    "xset -b",	-- Disable bell
    -- Enable numlock
-   "numlockx on",
+   --"numlockx on",
+   -- Replace caps-lock with ctrl
+   "setxkbmap -option ctrl:nocaps",
    -- Default browser
    "xdg-mime default " .. config.browser .. ".desktop " ..
       "x-scheme-handler/http " ..
@@ -25,5 +27,6 @@ os.execute(table.concat(execute, ";"))
 -- Spawn various X programs
 xrun("polkit-gnome-authentication-agent-1",
      "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1")
-xrun("pidgin", "pidgin -n")
+-- xrun("pidgin", "pidgin -n")
 xrun("NetworkManager Applet", "nm-applet")
+
