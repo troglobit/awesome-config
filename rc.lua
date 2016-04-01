a7307910b7eaed73a4da7b003b583b4bf4cf2705
Joachim Nilsson <troglobit@gmail.com>
2016-04-01 02:09:30 +0200

    Start terminals maximized
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- rc.lua
+++ rc.lua
@@ -45,7 +45,7 @@ end
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "x-terminal-emulator -m"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
