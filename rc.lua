fec9aef6852bc167972a8c031a6f4e4b941dc604
Joachim Nilsson <troglobit@gmail.com>
2016-10-03 00:21:41 +0200

    Add alternative xlock shortcut key: Win-Ctrl-l
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- rc.lua
+++ rc.lua
@@ -325,8 +325,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+--    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
+--    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
 
@@ -430,7 +430,8 @@ root.keys(globalkeys)
 
 -- Screen saver (XF86ScreenSaver)
 globalkeys = awful.util.table.join(globalkeys,
-	awful.key({}, "Pause", function () awful.util.spawn("xautolock -locknow") end))
+	awful.key({}, "Pause",                function () awful.util.spawn("xautolock -locknow") end),
+	awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("xautolock -locknow") end))
 root.keys(globalkeys)
 
 -- ...
