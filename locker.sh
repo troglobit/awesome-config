b1d34d8910f89fe1ba09a15bb332511b866e0a04
Joachim Nilsson <troglobit@gmail.com>
2016-10-13 08:30:20 +0200

    Actually lock the screen, but after 15 mins
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- locker.sh
+++ locker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 exec xautolock -detectsleep \
-  -time 3 -locker "gnome-screensaver-command -a" \
-  -notify 30 \
-  -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
+     -time 15 -locker "gnome-screensaver-command -l" \
+     -notify 30 \
+     -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
